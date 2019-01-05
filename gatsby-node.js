6ef63d77d70ae011c920242f726e2fb2eb9d613e
Michal Piechowiak <misiek.piechowiak@gmail.com>
2019-01-06 00:20:18 +0100

    init

---
+++ gatsby-node.js
@@ -0,0 +1,40 @@
+const createTemporaryMockNodes = ({ emitter, actions}) => {
+  const node = {
+    id: 'mocked',
+    parent: null,
+    children: [],
+    myField: 'yup',
+    internal: {
+      type: 'MockedNodeType',
+      contentDigest: 'foo'
+    }
+  }
+
+  actions.createNode(node)
+
+  const onSchemaUpdate = () => {
+    actions.deleteNode({ node })
+
+    // poor man's "once"
+    emitter.off(`SET_SCHEMA`, onSchemaUpdate)
+  }
+
+  // we will listen to when schema is set,
+  // so we can immediately remove mocked nodes
+  // as types are already produced, we don't need them anymore
+  // THIS IS HACKY - emitter is considered more of a private API
+  // and listening to internal SET_SCHEMA message might be begging
+  // for problems
+  emitter.on(`SET_SCHEMA`, onSchemaUpdate)
+}
+
+// that's pretty normal spot to create nodes
+exports.sourceNodes = ({ emitter, actions }) => {
+  createTemporaryMockNodes({ emitter, actions })
+}
+
+// this is just first API hook after "createPages" hook
+// and before regenerating schema
+exports.onPreExtractQueries = ({ emitter, actions }) => {
+  createTemporaryMockNodes({ emitter, actions })
+}
